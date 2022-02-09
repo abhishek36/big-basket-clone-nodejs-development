f95fc25da606d04e9a0eaea943030846e0e14e5b
abhishek36 <akamble9913@gmail.com>
2022-02-09 13:25:30 +0530

    first commit

---
+++ src/controllers/basket.controller.js
@@ -0,0 +1,25 @@
+//import Basket model here
+
+const addBasket = async (req, res) => {
+  //addBasket api logic here
+};
+
+const getBasketByUserId = async (req, res) => {
+  //getBasketByUserId api logic here
+};
+
+const getBasketItemById = async (req, res) => {
+  //getBasketItemById api logic here
+};
+
+const deleteBasketItemById = async (req, res) => {
+  //deleteBasketItemById api logic here
+};
+
+const BasketController = {
+  addBasket,
+  getBasketByUserId,
+  getBasketItemById,
+  deleteBasketItemById,
+};
+module.exports = BasketController;
